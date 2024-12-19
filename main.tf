c56bcaaefa4ee6780363534b74ec855e9ff49e5a
smaniprasad12 <104119726+smaniprasad12@users.noreply.github.com>
2024-12-20 01:25:03 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,39 @@
+resource "aws_launch_template" "web_server_as" {
+    name = "myproject"
+    image_id           = "ami-0ddc798b3f1a5117e"
+    instance_type = "t2.micro"
+    key_name = "mustafakey-pair"
+    
+}
+   
+
+
+  resource "aws_elb" "web_server_lb"{
+     name = "web-server-lb"
+     security_groups = [aws_security_group.web_server.id]
+     subnets = ["subnet-0883b34c988bcbf20", "subnet-0f4f4322d734fcc38"]
+     listener {
+      instance_port     = 8000
+      instance_protocol = "http"
+      lb_port           = 80
+      lb_protocol       = "http"
+    }
+    tags = {
+      Name = "terraform-elb"
+    }
+  }
+resource "aws_autoscaling_group" "web_server_asg" {
+    name                 = "web-server-asg"
+    min_size             = 1
+    max_size             = 3
+    desired_capacity     = 2
+    health_check_type    = "EC2"
+    load_balancers       = [aws_elb.web_server_lb.name]
+    availability_zones    = ["us-east-1a", "us-east-1b"] 
+    launch_template {
+        id      = aws_launch_template.web_server_as.id
+        version = "$Latest"
+      }
+    
+  }
+
