548f7b9882bc55e148eede0964191baff27c52c6
smaniprasad12 <104119726+smaniprasad12@users.noreply.github.com>
2024-12-20 02:07:06 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -4,29 +4,31 @@ resource "aws_s3_bucket" "one" {
 
 resource "aws_s3_bucket_ownership_controls" "two" {
   bucket = aws_s3_bucket.one.id
+
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
 }
 
-resource "aws_s3_bucket_acl" "three" {
-  depends_on = [aws_s3_bucket_ownership_controls.two]
+resource "aws_s3_bucket_acl" "four" {
+  depends_on = [aws_s3_bucket.one]  # Direct dependency on the bucket
 
   bucket = aws_s3_bucket.one.id
   acl    = "private"
 }
 
-resource "aws_s3_bucket_versioning" "three" {
-bucket = aws_s3_bucket.one.id
-versioning_configuration {
-status = "Enabled"
-}
+resource "aws_s3_bucket_versioning" "five" {
+  bucket = aws_s3_bucket.one.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
 
 terraform {
-backend "s3" {
-region = "us-east-1"
-bucket = "ruksar.devops.project.bucket"
-key = "prod/terraform.tfstate"
-}
+  backend "s3" {
+    region = "us-east-1"
+    bucket = "ruksar.devops.project.bucket"
+    key    = "prod/terraform.tfstate"
+  }
 }
