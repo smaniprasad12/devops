a6bda9d7dc4edf6b7ddfb9305549d79078566226
smaniprasad12 <104119726+smaniprasad12@users.noreply.github.com>
2024-12-20 01:36:12 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "one" {
-  bucket = "mani.devops.project.bucket"
+  bucket = "maniprasads.devops.project.bucket"
 }
 
 resource "aws_s3_bucket_ownership_controls" "two" {
@@ -26,7 +26,7 @@ status = "Enabled"
 terraform {
 backend "s3" {
 region = "us-east-1"
-bucket = "mani.devops.project.bucket"
+bucket = "maniprasads.devops.project.bucket"
 key = "prod/terraform.tfstate"
 }
 }
